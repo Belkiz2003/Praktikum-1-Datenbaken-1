d548bf13ed76d44ef47105b4103b60cc855b3e9f
Tobias Handwerk <tobiashandwerk79@gmail.com>
2025-12-11 23:51:58 +0100

    Aufgabe 2 erledigt und kommentiert

--- quaries_1-11.sql
+++ quaries_1-11.sql
@@ -7,8 +7,7 @@ JOIN hoeren h ON s.MatrNr = h.MatrNr
 JOIN Vorlesungen v ON h.VorlNr = v.VorlNr 
 WHERE v.Titel = 'Ethik';
 
--- kürzere und vermutlich schnellere alternative:
-
+-- kürzere und vermutlich schnellere alternative zu 1:
 SELECT matrnr 
 FROM hoeren 
 JOIN vorlesungen ON vorlesungen.vorlnr = hoeren.vorlnr 
@@ -60,48 +59,54 @@ ORDER BY Anzahl_Pruefungen DESC;
 
 -- 6. Finden Sie den/die Professor(en) (Name ausgeben) mit den meisten Assistenten.
 
-SELECT professoren.name --, 
---count(boss) as assistenten 
+SELECT professoren.name --, -- <--- Falls gewünscht auskommentieren: zeigt dann auch noch die Anzahl der Assistenten an
+--count(boss) as assistenten -- <--- Falls gewünscht auskommentieren: zeigt dann auch noch die MenAnzahlge der Assistenten an
 FROM assistenten 
 JOIN professoren ON professoren.persnr=assistenten.boss 
 GROUP BY professoren.name 
-HAVING count(boss) = (
-	SELECT count(boss) as assistenten 
-	FROM assistenten 
+HAVING count(boss) = ( -- Dieser Teil findet die größte Anzahl an Assistenten, um die dann mit allen Professoren zu vergleichen.
+	SELECT count(boss) as assistenten -- Diese Query alleine würde schon ausreichen um !DEN! Professor mit den meisten Assistenten zu ermitteln,
+	FROM assistenten                  -- aber es ist nach allen gefragt, muss dieser wert noch weiter mit allen abgeglichen werden.
 	JOIN professoren ON professoren.persnr=assistenten.boss 
-	GROUP BY professoren.name ORDER BY assistenten DESC LIMIT 1
+	GROUP BY professoren.name ORDER BY assistenten DESC LIMIT 1 --
 );
 
 -- 7. Welche Studierenden hören alle Vorlesungen?
 
 SELECT studenten.name,studenten.matrnr
 FROM studenten 
-JOIN hoeren ON hoeren.matrnr=studenten.matrnr 
+JOIN hoeren ON hoeren.matrnr=studenten.matrnr -- Join um den studenten mit seinen gehörten Vorlesungen zu verknüpfen. Ist nur für den Namen notwendig.
 GROUP BY studenten.name,studenten.matrnr
 
-HAVING count(*) = (
-	SELECT count(*) 
+HAVING count(*) = (  -- Anzahl aller Vorlesungen vergleichen mit der Anzahl der Vorlesungen, welche vom jeweiligen Studenten gehört werden
+	SELECT count(*)  -- Anzahl der Vorlesungen zählen
 	FROM vorlesungen
 );
 
 -- 8. Wie oft wurde eine Prüfung mit der Note 1 oder 2 bewertet?
 
-SELECT count(*) FROM pruefen WHERE note <=2 AND note >=1;
+SELECT count(*) FROM pruefen WHERE note <=2 AND note >=1; -- Kommentare nötig?
 
 -- 9. Erstellen Sie eine Übersicht, in der die MatrNr und der Name der Studierenden zusammen mit der
 -- von ihnen erreichten Durchschnittsnote sowie dem dazugehörigen Varianz-Wert angeben werden.
 
-SELECT name,inf.matrnr,inf.avg,inf.var 
-FROM (SELECT matrnr, AVG(note), (MAX(note)-MIN(note)) as var FROM pruefen GROUP BY matrnr) as inf 
-JOIN studenten ON studenten.matrnr=inf.matrnr;
+SELECT name,inf.matrnr,inf.avg,inf.var --endgültiger select 
+
+FROM (
+	SELECT matrnr, AVG(note), (MAX(note)-MIN(note)) as var -- Alle Werte erfassen
+	FROM pruefen 
+	GROUP BY matrnr -- auf studis gruppieren
+) as inf -- benennen zur identifizierung
+
+JOIN studenten ON studenten.matrnr=inf.matrnr; -- Join um die matrikelnummern mit Namen zu versehen
 
 -- 10. Gibt es Namen von Personen, die in mindestens zwei verschiedenen Tabellen auftreten?
 
 SELECT 
 CASE 
-	WHEN EXISTS (
+	WHEN EXISTS (      -- Falls welche gefunden wird 1, sonst 0 zurückgegeben, gleicht TRUE / FALSE
 		SELECT a.name, p.name, s.name 
-		FROM assistenten as a,professoren as p,studenten as s 
+		FROM assistenten as a,professoren as p,studenten as s                   -- Hier werden namen rausgesucht
 		WHERE a.name = p.name OR a.name = s.name OR p.name = s.name
 	) 
 	THEN 1
@@ -116,17 +121,17 @@ SELECT * FROM
 
 (
 SELECT DISTINCT v1.vorgaenger,v2.nachfolger as zielmodul 
-FROM voraussetzen as v1 
+FROM voraussetzen as v1                                            -- Alle indirekten Vorgänger
 RIGHT JOIN voraussetzen as v2 ON v1.nachfolger=v2.vorgaenger 
 
-UNION
+UNION     -- Zusammenfassung beider
 
 SELECT DISTINCT v2.vorgaenger,v2.nachfolger as zielmodul 
-FROM voraussetzen as v1 
+FROM voraussetzen as v1                                            -- Alle direkten Vorgänger
 RIGHT JOIN voraussetzen as v2 ON v1.nachfolger=v2.vorgaenger
 )
 
-WHERE vorgaenger IS NOT NULL 
-ORDER BY zielmodul
+WHERE vorgaenger IS NOT NULL  -- Null ausfiltern
+ORDER BY zielmodul -- Sortieren (deutlich bessere übersicht)
 
 -- Indirekt nur bis zur zweiten Ebene, wahrscheinlich rekursive Lösung geforderdert?
